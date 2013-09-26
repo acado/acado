ef15cba36b030d623232795238d24ca4a79e3287
Milan Vukov <mvukov@gmail.com>
2013-09-26 20:30:27 +0200

    Added two more examples
    … And fixed the .gitignore in examples folder.

---
+++ examples/code_generation/mpc_mhe/crane_mhe.cpp
@@ -0,0 +1,148 @@
+/*
+ *    This file is part of ACADO Toolkit.
+ *
+ *    ACADO Toolkit -- A Toolkit for Automatic Control and Dynamic Optimization.
+ *    Copyright (C) 2008-2013 by Boris Houska, Hans Joachim Ferreau,
+ *    Milan Vukov, Rien Quirynen, KU Leuven.
+ *    Developed within the Optimization in Engineering Center (OPTEC)
+ *    under supervision of Moritz Diehl. All rights reserved.
+ *
+ *    ACADO Toolkit is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    ACADO Toolkit is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with ACADO Toolkit; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include <acado_toolkit.hpp>
+
+USING_NAMESPACE_ACADO
+
+int main( void )
+{
+	//
+	// OCP parameters
+	//
+	// Sampling time
+	double Ts = 0.01;
+	// Number of shootin' intervals
+	int N = 20;
+	// Number of integrator steps per shootin' interval
+	int Ni = 1;
+
+	//
+	// DEFINE THE VARIABLES
+	//
+	DifferentialState	xT;	// the trolley position
+	DifferentialState	vT;	// the trolley velocity
+	IntermediateState	aT;	// the trolley acceleration
+
+	DifferentialState	xL;	// the cable length
+	DifferentialState	vL;	// the cable velocity
+	IntermediateState	aL;	// the cable acceleration
+
+	DifferentialState 	phi;	// the angle
+	DifferentialState	omega;	// the angular velocity
+
+	DifferentialState	uT;		// trolley velocity control
+	DifferentialState	uL;		// cable velocity control
+
+	Control				duT;	// trolley accel control
+	Control				duL;	// cable accel control
+
+	//
+	// DEFINE THE MODEL PARAMETERS
+	//
+	const double		tau1	= 0.012790605943772;
+	const double		a1		= 0.047418203070092;
+	const double		tau2	= 0.024695192379264;
+	const double		a2		= 0.034087337273386;
+	const double		g		= 9.81;
+//	const double		c		= 0.0;
+//	const double		m		= 1.3180;
+
+	//
+	// DEFINE THE MODEL EQUATIONS
+	//
+	DifferentialEquation	f;
+	aT = -1.0 / tau1 * vT + a1 / tau1 * uT;
+	aL = -1.0 / tau2 * vL + a2 / tau2 * uL;
+
+	f << dot(xT) == vT;
+	f << dot(vT) == aT;
+	f << dot(xL) == vL;
+	f << dot(vL) == aL;
+	f << dot(phi) == omega;
+	f << dot(omega)
+			== -(g * sin(phi) + a1 * duT * cos(phi) + 2 * vL * omega) / xL;
+	f << dot(uT) == duT;
+	f << dot(uL) == duL;
+
+	//
+	// MHE PROBLEM FORMULATION
+	//
+	OCP ocp(0.0, N * Ts, N);
+
+	ocp.subjectTo( f );
+
+	// Measurement function h(x, u) on first N nodes
+	Function h;
+
+	h << xT << xL << phi << uT << uL << duT << duL;
+
+	// Weighting matrices and measurement functions
+	Matrix S = eye( 7 );
+//	S(0,0) = 16.5;
+//	S(1,1) = 23.9;
+//	S(2,2) = 25.1;
+//	S(3,3) = 12.1;
+//	S(4,4) = 119.4;
+//	S(5,5) = 45.0;
+//	S(6,6) = 1.2;
+//	S(7,7) = 0.4;
+
+	Function hN;
+	hN << xT << xL << phi << uT << uL;
+
+	Matrix SN = eye( 5 );
+	SN(0,0) = S(0,0);
+	SN(1,1) = S(1,1);
+	SN(2,2) = S(2,2);
+	SN(3,3) = S(3,3);
+	SN(4,4) = S(4,4);
+
+	ocp.minimizeLSQ(S, h);
+	ocp.minimizeLSQEndTerm(SN, hN);
+
+	OCPexport mhe( ocp );
+
+	mhe.set(INTEGRATOR_TYPE, INT_RK4);
+	mhe.set(NUM_INTEGRATOR_STEPS, N * Ni);
+
+	mhe.set(HESSIAN_APPROXIMATION, GAUSS_NEWTON);
+	mhe.set(DISCRETIZATION_TYPE, MULTIPLE_SHOOTING);
+
+	mhe.set(HOTSTART_QP, YES);
+
+	// NOTE: This is crucial for export of MHE!
+	mhe.set(SPARSE_QP_SOLUTION, CONDENSING);
+	mhe.set(FIX_INITIAL_STATE, NO);
+
+//	mhe.set( LEVENBERG_MARQUARDT, 1e-10 );
+
+	if (mhe.exportCode("crane_mhe_export") != SUCCESSFUL_RETURN)
+		exit( EXIT_FAILURE );
+
+	mhe.printDimensionsQP( );
+
+	return EXIT_SUCCESS;
+}
---
+++ examples/code_generation/mpc_mhe/pendulum_dae_nmpc.cpp
@@ -0,0 +1,134 @@
+/*
+ *    This file is part of ACADO Toolkit.
+ *
+ *    ACADO Toolkit -- A Toolkit for Automatic Control and Dynamic Optimization.
+ *    Copyright (C) 2008-2013 by Boris Houska, Hans Joachim Ferreau,
+ *    Milan Vukov, Rien Quirynen, KU Leuven.
+ *    Developed within the Optimization in Engineering Center (OPTEC)
+ *    under supervision of Moritz Diehl. All rights reserved.
+ *
+ *    ACADO Toolkit is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    ACADO Toolkit is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with ACADO Toolkit; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include <acado_toolkit.hpp>
+
+USING_NAMESPACE_ACADO
+
+int main(int argc, char * const argv[ ])
+{
+	//
+	// Variables
+	//
+
+	DifferentialState	x, y, w, dx, dy, dw;
+	AlgebraicState		mu;
+	Control				F;
+
+	IntermediateState	c, dc;
+
+	const double		m = 1.0;
+	const double		mc = 1.0;
+	const double		L = 1.0;
+	const double		g = 9.81;
+	const double		p = 5.0;
+
+	c	= 0.5 * ((x - w) * (x - w) + y * y - L * L);
+	dc	= dy * y + (dw - dx) * (w - x);
+
+	//
+	// Differential algebraic equation
+	//
+
+	DifferentialEquation f;
+
+	f << 0 == dot( x ) - dx;
+	f << 0 == dot( y ) - dy;
+	f << 0 == dot( w ) - dw;
+
+	f << 0 == m * dot( dx ) + (x - w) * mu;
+	f << 0 == m * dot( dy ) + y * mu + m * g;
+	f << 0 == mc * dot( dw ) + (w - x) * mu  - F;
+
+	f << 0 == (x - w) * dot( dx ) + y * dot( dy ) + (w - x) * dot( dw )
+					- (-p * p * c - 2 * p * dc - dy * dy - (dw - dx) * (dw - dx));
+
+	//
+	// Weighting matrices and reference functions
+	//
+
+	Function rf;
+	Function rfN;
+
+	rf << x << y << w << dx << dy << dw << F;
+	rfN << x << y << w << dx << dy << dw;
+
+	Matrix S = eye( rf.getDim() );
+	Matrix SN = eye( rfN.getDim() );
+
+	//
+	// Optimal Control Problem
+	//
+
+	const int N  = 10;
+	const int Ni = 4;
+	const double Ts = 0.1;
+
+	OCP ocp(0, N * Ts, N);
+
+	ocp.subjectTo( f );
+
+	ocp.minimizeLSQ(S, rf);
+	ocp.minimizeLSQEndTerm(SN, rfN);
+
+	ocp.subjectTo(-20 <= F <= 20);
+//	ocp.subjectTo( -5 <= x <= 5 );
+
+	//
+	// Export the code:
+	//
+	OCPexport mpc( ocp );
+
+	mpc.set(HESSIAN_APPROXIMATION, GAUSS_NEWTON);
+	mpc.set(DISCRETIZATION_TYPE, MULTIPLE_SHOOTING);
+
+	mpc.set(INTEGRATOR_TYPE, INT_IRK_RIIA3);
+	mpc.set(NUM_INTEGRATOR_STEPS, N * Ni);
+
+	mpc.set(SPARSE_QP_SOLUTION, FULL_CONDENSING);
+//	mpc.set(SPARSE_QP_SOLUTION, CONDENSING);
+	mpc.set(QP_SOLVER, QP_QPOASES);
+//	mpc.set(MAX_NUM_QP_ITERATIONS, 20);
+	mpc.set(HOTSTART_QP, YES);
+
+//	mpc.set(SPARSE_QP_SOLUTION, SPARSE_SOLVER);
+//	mpc.set(QP_SOLVER, QP_FORCES);
+
+//	mpc.set(LEVENBERG_MARQUARDT, 1.0e-10);
+
+	mpc.set(GENERATE_TEST_FILE, NO);
+	mpc.set(GENERATE_MAKE_FILE, NO);
+	mpc.set(GENERATE_MATLAB_INTERFACE, YES);
+
+//	mpc.set(USE_SINGLE_PRECISION, YES);
+//	mpc.set(CG_USE_OPENMP, YES);
+
+	if (mpc.exportCode( "pendulum_dae_nmpc_export" ) != SUCCESSFUL_RETURN)
+		exit( EXIT_FAILURE );
+
+	mpc.printDimensionsQP( );
+
+	return EXIT_SUCCESS;
+}
