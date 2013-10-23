d465b9b56039bd81088a05ade72fb3a6b4ee71dd
Milan Vukov <mvukov@gmail.com>
2013-10-23 14:05:37 +0200

    Cleaned the code-deploy script

--- misc/deploy_code.sh
+++ misc/deploy_code.sh
@@ -3,12 +3,10 @@
 # Name of the archive
 ZIP_FILE=acadotoolkit-current-$TRAVIS_BRANCH.zip
 
-# Deploy only if the code is pushed to the "blessed" remote
+# Deploy only if the code is pushed to the "blessed" remote and if a g++ compiler is being used
 if [ "$TRAVIS_REPO_SLUG" == "acado/acado" ] && [ "$CXX" == "g++" ]; then
 	# Make an archive of the current travis-ci checked commit
 	git archive -o $ZIP_FILE $TRAVIS_COMMIT
 	# Deploy the archive to the website
 	curl -T $ZIP_FILE -u $FTP_USER:$FTP_PASS ftp://ftp.acadotoolkit.org/zip/$ZIP_FILE
-else
-	echo "Dude, you cannot deploy, the code is not pushed to the blessed remote!"
 fi
