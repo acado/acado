23af2063b6e0caf34f6f8dd1a19bb1409ceff9f3
Milan Vukov <mvukov@gmail.com>
2013-10-25 14:08:07 +0200

    Polishing Travis-CI scripts

--- misc/travis_before_install.sh
+++ misc/travis_before_install.sh
@@ -3,7 +3,7 @@
 ################################################################################
 #
 # Description:
-#	A script for updating of the Doxygen documentation
+#	Install necessary dependencies for Travis-CI
 #
 # Authors:
 #	Milan Vukov, milan.vukov@esat.kuleuven.be
@@ -16,19 +16,12 @@
 #
 ################################################################################
 
-# Update documentation only if:
+# Run only if:
 # - the code is pushed to the "blessed" remote and
 # - a g++ compiler is being used and
 # - and if we pushed to the stable branch
 if [ "$TRAVIS_REPO_SLUG" == "acado/acado" ] && [ "$CXX" == "g++" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-	# Move to the build folder
-	cd build
-	# Make documentation
-	make -j2 doc
-	# Change to doc folder
-	cd ../doc
-	# Synchronize the documentation folders
-	rsync -avzP --delete -e 'ssh -o StrictHostKeyChecking' html "mvukov,acado@web.sourceforge.net:/home/groups/a/ac/acado/htdocs/doc"
-	# Move back to the root folder
-	cd ..
+	# Install necessary deps
+	sudo apt-get update -qq
+	sudo apt-get install -qq doxygen graphviz
 fi
