b74b8203d7db464c77f73bee28ca25b74b1e94e0
Milan Vukov <mvukov@gmail.com>
2013-10-25 13:32:52 +0200

    Added script for automatic update of documentation

--- misc/deploy_code.sh
+++ misc/deploy_code.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+################################################################################
+#
+# Description:
+#	A script for upload of the source code to the acadotoolkit.org web-space
+#
+# Authors:
+#	Milan Vukov, milan.vukov@esat.kuleuven.be
+#
+# Year:
+#	2013.
+#
+# Usage:
+#	- This script is automatically called from Travis-CI script
+#
+################################################################################
+
 # Name of the archive
 ZIP_FILE=acadotoolkit-current-$TRAVIS_BRANCH.zip
 
-# Deploy only if the code is pushed to the "blessed" remote and if a g++ compiler is being used
+# Deploy only if:
+# - the code is pushed to the "blessed" remote and
+# - a g++ compiler is being used
 if [ "$TRAVIS_REPO_SLUG" == "acado/acado" ] && [ "$CXX" == "g++" ]; then
 	# Make an archive of the current travis-ci checked commit
 	git archive -o $ZIP_FILE $TRAVIS_COMMIT
