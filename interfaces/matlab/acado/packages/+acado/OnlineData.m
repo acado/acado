ca94fc22c878453eef8b021dce67c3129e6fac4e
Rien Quirynen <rien.quirynen@gmail.com>
2013-12-21 17:46:26 +0100

    added OnlineData class to Matlab interface

---
+++ interfaces/matlab/acado/packages/+acado/OnlineData.m
@@ -0,0 +1,85 @@
+%OnlineData
+%
+%  Usage:
+%    >> OnlineData(name);
+%    >> OnlineData name;                          SHORTHAND
+%    >> OnlineDatas name1 name2 name3 ....        SHORTHAND
+%
+%  OnlineDatas:
+%    name 	   A valid matlab variable name [STRING]
+%
+%  Example:
+%    >> OnlineData p;
+%
+%  Licence:
+%    This file is part of ACADO Toolkit  - (http://www.acadotoolkit.org/)
+%
+%    ACADO Toolkit -- A Toolkit for Automatic Control and Dynamic Optimization.
+%    Copyright (C) 2008-2009 by Boris Houska and Hans Joachim Ferreau, K.U.Leuven.
+%    Developed within the Optimization in Engineering Center (OPTEC) under
+%    supervision of Moritz Diehl. All rights reserved.
+%
+%    ACADO Toolkit is free software; you can redistribute it and/or
+%    modify it under the terms of the GNU Lesser General Public
+%    License as published by the Free Software Foundation; either
+%    version 3 of the License, or (at your option) any later version.
+%
+%    ACADO Toolkit is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%    Lesser General Public License for more details.
+%
+%    You should have received a copy of the GNU Lesser General Public
+%    License along with ACADO Toolkit; if not, write to the Free Software
+%    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+%
+%    Author: Rien Quirynen
+%    Date: 2013
+%
+classdef OnlineData < acado.Variable
+    properties(SetAccess='private')
+        
+    end
+    
+    methods
+        function obj = OnlineData(name)
+            if nargin > 0
+                global ACADO_;
+                
+                if (isvarname(name) ~= 1)
+                    error( 'ERROR: The variable name you have set is not a valid matlab variable name. A valid variable name is a character string of letters, digits, and underscores, totaling not more than namelengthmax characters and beginning with a letter.' );
+                end
+                
+                obj.name = name;
+                
+                ACADO_.helper.addP(obj);
+                ACADO_.helper.addInstruction(obj);
+            end
+        end
+        
+        function jac = jacobian(obj, var)
+            if ~isvector(obj)
+                error('A jacobian can only be computed of a vector function.');
+            end
+            for j = 1:length(obj)
+                for i = 1:length(var)
+                    var(i) = getExpression(var(i));
+                    if isa(var(i), 'acado.Variable')
+                        jac(j,i) = acado.DoubleConstant(double(isa(var(i), 'acado.OnlineData') && strcmp(obj(j).name, var(i).name)));
+                    else
+                        error('A jacobian can only be computed with respect to a variable.');
+                    end
+                end
+            end
+        end
+        
+        function getInstructions(obj, cppobj, get)
+            if (get == 'FB')
+                fprintf(cppobj.fileMEX,sprintf('    OnlineData %s; \n', obj.name));
+            end    
+        end
+        
+    end
+    
+end
+
