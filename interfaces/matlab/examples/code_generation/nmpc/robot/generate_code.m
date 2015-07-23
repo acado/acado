24355e125211a12c2339d1e5c655a717b47e460c
Rien Quirynen <rien.quirynen@gmail.com>
2015-07-23 23:50:34 +0200

    minor update robot example

--- interfaces/matlab/examples/code_generation/nmpc/robot/generate_code.m
+++ interfaces/matlab/examples/code_generation/nmpc/robot/generate_code.m
@@ -17,7 +17,7 @@ B1(1,2) = 0.5;
 B1(2,1) = 1.0/base;
 B1(2,2) = -1.0/base;
 
-f = [ v*cos(theta); v*sin(theta) ];
+f = [ dot(xC) == v*cos(theta); dot(yC) == v*sin(theta) ];
 
 %% SIMexport
 fprintf('----------------------------\n         SIMexport         \n----------------------------\n');
