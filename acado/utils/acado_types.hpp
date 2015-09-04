bb60b016acf5d244968c14a0e454216993e339f0
Rien Quirynen <rien.quirynen@gmail.com>
2015-09-04 13:54:32 +0200

    LIFTED_GRADIENT_UPDATE added to options list

--- acado/utils/acado_types.hpp
+++ acado/utils/acado_types.hpp
@@ -341,6 +341,7 @@ enum OptionsName
 	CG_HARDCODE_CONSTRAINT_VALUES,				/**< Enable/disable hard-coding of the constraint values. */
 	IMPLICIT_INTEGRATOR_MODE,					/**< This determines the mode of the implicit integrator (see enum ImplicitIntegratorMode). */
 	LIFTED_INTEGRATOR_MODE,						/**< This determines the mode of lifting of the implicit integrator. */
+	LIFTED_GRADIENT_UPDATE,						/**< This determines whether the gradient will be updated, based on the lifted implicit integrator. */
 	IMPLICIT_INTEGRATOR_NUM_ITS,				/**< This is the performed number of Newton iterations in the implicit integrator. */
 	IMPLICIT_INTEGRATOR_NUM_ITS_INIT,			/**< This is the performed number of Newton iterations in the implicit integrator for the initialization of the first step. */
 	UNROLL_LINEAR_SOLVER,						/**< This option of the boolean type determines the unrolling of the linear solver (no unrolling recommended for larger systems). */
