ff8e15ff449d5a2aeb3dec1b57dd98a3ef63b04e
Rien Quirynen <rien.quirynen@gmail.com>
2016-02-25 11:18:32 +0100

    fixed double module names for regularization

--- acado/code_generation/export_exact_hessian_cn2.cpp
+++ acado/code_generation/export_exact_hessian_cn2.cpp
@@ -234,7 +234,7 @@ returnValue ExportExactHessianCN2::setupHessianRegularization( )
 	get(CG_MODULE_NAME, moduleName);
     
 	ExportVariable block( "hessian_block", NX+NU, NX+NU );
-	regularization = ExportFunction( moduleName + "_regularize", block );
+	regularization = ExportFunction( "regularize", block );
 	regularization.doc( "EVD-based regularization of a Hessian block." );
 	regularization.addLinebreak();
 
